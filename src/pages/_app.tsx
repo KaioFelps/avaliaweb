59fc383b14fd83c87bb440185471e917729b5d81
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-10-05 15:51:02 -0300

    style: main style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { MobileNavBar } from '@/components/MobileNavBar';
 import Sidebar from '@/components/Sidebar';
 import '@/styles/globals.css'
 import { theme } from '@/styles/theme';
@@ -6,16 +7,30 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const SideBarWidth = 224;
+  const SidesMargin = 48;
+  const MobileSidesMargin = 12;
+
   return (
     <ThemeProvider theme={theme}>
       <Head>
         <title>Sistema de Avaliação • UNICAP </title>
       </Head>
 
-      <main className="flex">
+      <div className="flex max-sm:flex-col">
+        <MobileNavBar />
         <Sidebar />
-        <Component {...pageProps} />
-      </main>
+
+        <main
+          className={`
+          max-w-6xl my-12 mx-auto
+          max-sm:w-[calc(100%_-_${MobileSidesMargin * 2}px)] max-sm:mt-0
+          min-[640px]:w-[calc(100%_-_${SidesMargin * 2}px_-_${SideBarWidth}px)]
+          `}
+        >
+          <Component {...pageProps} />
+        </main>
+      </div>
     </ThemeProvider>
   );
 }
