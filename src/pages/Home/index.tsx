2f6735cef10554e9ac80d262ea6e14b9ebe5912a
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-10-05 15:11:01 -0300

    feat: remove main and aside from home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -18,24 +18,21 @@ export default function Home() {
 
 
   return (
-    <main className="flex">
-      <Sidebar />
-      <div className="flex flex-col bg-bground h-screen w-screen pl-12 pr-28 pt-36">
-        <div className="mb-8">
-          <Text size="2xl" weight="semibold" fontFamily="sans" color="black">
-            Nome do aluno
-          </Text>
-          <Text size="xl" weight="normal" fontFamily="sans" color="black" >
-            {dataFormatada}
-          </Text>
-        </div>
-        <div >
-          <Card title="Tarefa 1" description="Esta é a descrição da tarefa 1." status="feito" />
-          <Card title="Tarefa 2" description="Esta é a descrição da tarefa 2." status="pendente" />
-          <Card title="Tarefa 3" description="Esta é a descrição da tarefa 3." status="incompleto" />
-        </div>
-
+    <div className="flex flex-col bg-bground h-screen w-screen pl-12 pr-28 pt-36">
+      <div className="mb-8">
+        <Text size="2xl" weight="semibold" fontFamily="sans" color="black">
+          Nome do aluno
+        </Text>
+        <Text size="xl" weight="normal" fontFamily="sans" color="black" >
+          {dataFormatada}
+        </Text>
       </div>
-    </main>
+      <div >
+        <Card title="Tarefa 1" description="Esta é a descrição da tarefa 1." status="feito" />
+        <Card title="Tarefa 2" description="Esta é a descrição da tarefa 2." status="pendente" />
+        <Card title="Tarefa 3" description="Esta é a descrição da tarefa 3." status="incompleto" />
+      </div>
+
+    </div>
   );
 }
